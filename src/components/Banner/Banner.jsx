6d000fb36cf366f66cb905067d2e57e399415927
GustavoEdson <ggugu9289@gmail.com>
2025-01-03 20:13:55 -0300

    update

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import "./Banner.css"
+
+const Banner = () => {
+    return (
+        <header className="banner">
+            <img src="/public/imagens/banner.png" alt="main banner image" />
+        </header>
+    )
+}
+
+export default Banner
