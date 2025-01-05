ec7db26b310b63c1ed4a7226a217be324b38ce74
GustavoEdson <ggugu9289@gmail.com>
2025-01-05 14:15:04 -0300

    feature/input-text

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 
 import Banner from './components/Banner/Banner'
+import InputText from './components/InputText'
 
 function App() {
 
   return (
     <div>
        <Banner />
+       <InputText label="Name" inputId="name" inputPlaceholder="Type your name"/>
+       <InputText label="Position"  inputId="position" inputPlaceholder="Type your position"/>
+       <InputText label="Image" inputId="image" inputPlaceholder="Insect your image"/>
     </div>
   )
 }
