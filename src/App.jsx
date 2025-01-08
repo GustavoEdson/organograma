4b4c68cef690cc3b6dfffa5524a04d05bba104c6
GustavoEdson <ggugu9289@gmail.com>
2025-01-08 20:20:40 -0300

    handle-form-submit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
+import { useState } from 'react'
 import Banner from './components/Banner'
 import Form from './components/Form'
 
 function App() {
 
+  const [collaborators, setCollaborators] = useState([])
+  
   return (
     <div>
        <Banner />
-       <Form />
-       
+       <Form onCollaborator={collaborator => setCollaborators([...collaborators, collaborator])}/>
     </div>
   )
 }
--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,9 +1,10 @@
 import "./Form.css"
-import InputText from "../InputText"
+import InputText from "../Input"
 import Select from "../Select"
 import Button from "../Button"
+import { useState } from "react"
 
-const Form = () => {
+const Form = ({onCollaborator}) => {
     const teams = [
         "Programmers",
         "UX/UI",
@@ -13,21 +14,52 @@ const Form = () => {
         "Innovation"
     ]
 
+    const [name, setName] = useState("")
+    const [position, setPosition] = useState("")
+    const [image, setImage] = useState("")
+    const [team, setTeam] = useState("")
+ 
     const handleSubmitForm = (e) => {
-        e.preventDefault()
-        console.log("dados!!!");
+        e.preventDefault() 
+        onCollaborator({
+            name,
+            position,
+            team,
+            image
+        })
         
     }
     
     return (
         <section className="form">
             <form onSubmit={(e) => handleSubmitForm(e)}>
-                <h2>Insect the information to make a collaborator card</h2>
-                <InputText isRequired={true} label="Name" inputId="name" inputPlaceholder="Type your name"/>
-                <InputText isRequired={true} label="Position"  inputId="position" inputPlaceholder="Type your position"/>
-                <InputText isRequired={false} label="Image" inputId="image" inputPlaceholder="Insect your image"/>
-                <Select name="Team" items={teams}/>
-                <Button>Create Card</Button>
+            <h2>Insect the information to make a collaborator card</h2>
+            <InputText 
+                isRequired={true} 
+                label="Name" 
+                inputId="name" 
+                inputPlaceholder="Type your name"
+                value={name}
+                onChangeInput={value => setName(value)}
+            />
+            <InputText 
+                isRequired={true} 
+                label="Position"  
+                inputId="position" 
+                inputPlaceholder="Type your position"
+                value={position}
+                onChangeInput={value => setPosition(value)}
+            />
+            <InputText 
+                isRequired={false} 
+                label="Image" 
+                inputId="image" 
+                inputPlaceholder="Insect your image"
+                value={image}
+                onChangeInput={value => setImage(value)}
+            />
+            <Select name="Team" items={teams} onChangeSelect={value => setTeam(value)}/>
+            <Button>Create Card</Button>
             </form>
         </section>
     )
