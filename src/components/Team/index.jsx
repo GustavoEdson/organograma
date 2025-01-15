aab74c0ace61b3eb2f0b50f705ea82c66fc89803
GustavoEdson <ggugu9289@gmail.com>
2025-01-15 20:20:41 -0300

    update

--- src/components/Team/index.jsx
+++ src/components/Team/index.jsx
@@ -2,11 +2,20 @@ import Card from "../Card"
 import "./Team.css"
 
 const Team = ({name, primaryColor, secondaryColor, collaborators}) => {
+
    return (
-    <section className="team" style={{ backgroundColor: secondaryColor }}>
-        <h3 style={{ borderColor: primaryColor }} >{name}</h3>
-        {collaborators.map(collaborator => <Card />)}
-    </section>
+        <section className="team" style={{ backgroundColor: secondaryColor }}>
+            <h3 style={{ borderColor: primaryColor }}>{name}</h3>
+            <div className="collaborators">
+                {collaborators.map((collaborator, i)=> 
+                    <Card 
+                        key={i} 
+                        name={collaborator.name} 
+                        position={collaborator.position} 
+                        image={collaborator.image}
+                    />)}
+                </div>
+        </section>
    )
 } 
 
