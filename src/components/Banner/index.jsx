5e76746d4d71813f58b80c463376b26e78c8c269
GustavoEdson <ggugu9289@gmail.com>
2025-01-20 20:33:46 -0300

    fix-paths

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,7 @@ import "./Banner.css"
 const Banner = () => {
     return (
         <header className="banner">
-            <img src="/public/imagens/banner.png" alt="main banner image" />
+            <img src="/imagens/banner.png" alt="main banner image" />
         </header>
     )
 }
