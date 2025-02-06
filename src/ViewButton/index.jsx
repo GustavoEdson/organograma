1785ce794838f0c106001b59d3c88c31cad51a03
GustavoEdson <ggugu9289@gmail.com>
2025-02-05 21:21:40 -0300

    update/view-button

--- src/ViewButton/index.jsx
+++ src/ViewButton/index.jsx
@@ -1,11 +1,5 @@
 import "./ViewButton.css"
 import {IoIosAddCircle } from "react-icons/io";
 
-const ViewButton = ({onRemove}) => {
-    return (
-        <>  
-            <IoIosAddCircle size={50} className="view-btn" onClick={onRemove}/>
-        </>
-    )
-}
+const ViewButton = ({onRemove}) => <IoIosAddCircle size={50} className="view-btn" onClick={onRemove}/>
 export default ViewButton
